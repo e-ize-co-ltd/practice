0e3d48d596482e1d59095047001ac7fec3183204
nojiri-t-e-ize <103915123+nojiri-t-e-ize@users.noreply.github.com>
2023-09-15 11:14:53 +0900

    Update Test1.java staging test

--- test1-1/src/main/java/com/example/demo/Test1.java
+++ test1-1/src/main/java/com/example/demo/Test1.java
@@ -1,11 +1,9 @@
 package com.example.demo;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class Test1 {
-
+	// コメントアウト
 	@GetMapping("/test1")
 	public String write1() {
 		return "Hello World!-Welcome!"; 
